640c5a98350d516c7423e441e99934d9b9bd1c9e
Harry Collins <harrycollins@Harrys-MacBook-Air.local>
2026-03-24 17:56:18 +0400

    Trim radio station list

--- shared/data/radio-stations.ts
+++ shared/data/radio-stations.ts
@@ -133,12 +133,6 @@ export const radioStations: RadioStation[] = [
     streamUrl: "https://static-media.streema.com/media/cache/fb/4f/fb4f3ced8e778b4a7e1a52ca2a4faa28.jpg",
     logoUrl: "https://streema.com/radios/NBS_89Smart_FM#",
  },
- /*{
-    id: "vision-radio-89-1",
-    name: "Vision Radio 89.1",
-    streamUrl: "https://stream-158.zeno.fm/rmtdek37zxhvv?zt=eyJhbGciOiJIUzI1NiJ9.eyJzdHJlYW0iOiJybXRkZWszN3p4aHZ2IiwiaG9zdCI6InN0cmVhbS0xNTguemVuby5mbSIsInJ0dGwiOjUsImp0aSI6InRzQmRXX3gxUzdLYUxxOWY3ME5VdEEiLCJpYXQiOjE3MzczMTM3MTQsImV4cCI6MTczNzMxMzc3NH0.C1dlaRqPtSm-KKRVIjoT-KB-dT8XCIFhAPIMJXvIj_o?n=cbb50e70d4cf9fc599c8",
-    logoUrl: "https://radio.menu/images/1/visionradio-co-ug-vision-radio-89-1-mbarara-89-1-f/webp/",
-},*/
  {
     id: "open-gate-fm-mbale",
     name: "Open Gate FM Mbale",
@@ -162,73 +156,25 @@ export const radioStations: RadioStation[] = [
     name: "89.7 FM Voice of Kyankwanzi",
     streamUrl: "https://stream.zeno.fm/eyzf4ddwqcmvv?n=d7718804b7fb568e6495",
     logoUrl: "https://radio.menu/images/1/zeno-fm-89-7-fm-voice-of-kyankwanzi/webp/",
- },/*
- {
-    id: "galaxy-fm-100-2",
-    name: "Galaxy 100.2 FM",
-    streamUrl: "https://api.instant.audio/data/streams/132/100-2-galaxy-fm",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/100-2-galaxy-fm.png",
- },*/
+ },
  {
     id: "akaboozi-fm",
     name: "Akaboozi 87.9 FM",
     streamUrl: "http://p.radios.streema.com/radios/play/Akaboozi_FM_87.9",
     logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/akaboozi-fm.png",
- },/*
- {
-    id: "alpha-fm-kampala",
-    name: "Alpha FM Uganda",
-    streamUrl: "https://api.instant.audio/data/streams/132/alpha-fm-kampala-uganda",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/alpha-fm-kampala-uganda.png",
- },*/
+ },
  {
     id: "arua-one-fm-88-7",
     name: "Arua One FM 88.7",
     streamUrl: "https://stream.hydeinnovations.com:2020/stream/aruaone/stream/1/",
     logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/arua-one-fm.png",
- },/*
- {//
-    id: "boona-fm-91-8",
-    name: "91.8 Boona FM",
-    streamUrl: "https://api.instant.audio/data/streams/132/boona",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/boona.png",
  },
- {//
-    id: "dembe-fm-90-4",
-    name: "Dembe FM 90.4",
-    streamUrl: "http://p.radios.streema.com/radios/play/Dembe_FM",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/dembe-fm.png",
- },
- {//
-    id: "mega-fm-102",
-    name: "Mega FM 102",
-    streamUrl: "https://api.instant.audio/data/streams/132/mega",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/mega.png",
- },
- {//
-    id: "point-fm-mubende",
-    name: "Point FM Mubende",
-    streamUrl: "https://api.instant.audio/data/streams/132/point-fm",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/point-fm.png",
- },*/
  {
     id: "radio-city-97-fm",
     name: "Radio City 97 FM",
     streamUrl: "https://api.instant.audio/data/streams/132/radiocity",
     logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/radiocity.png",
- },/*
- {//
-    id: "radio-sapientia-94-4",
-    name: "Radio Sapientia 94.4 FM",
-    streamUrl: "https://api.instant.audio/data/streams/132/sapientia",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/sapientia.png",
  },
- {//not yet
-    id: "voice-of-teso-fm",
-    name: "Voice of Teso Radio FM",
-    streamUrl: "https://api.instant.audio/data/streams/132/voice-of-teso",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/voice-of-teso.png",
- },*/
  {
     id: "kasese-guide-radio-100-5",
     name: "Kasese Guide Radio 100.5",
@@ -240,61 +186,25 @@ export const radioStations: RadioStation[] = [
     name: "Acts Radio Uganda",
     streamUrl: "https://live.ionradios.com:1315/stream",
     logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/acts.png",
- },/*
- {
-    id: "blithe-fm",
-    name: "Blithe FM",
-    streamUrl: "https://api.instant.audio/data/streams/132/blithe",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/blithe.png",
- },*/
+ },
  {
     id: "freedom-experience-radio",
     name: "Freedom Experience Radio",
     streamUrl: "https://live.ionradios.com:1045/stream",
     logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/freedom-experience.png",
- },/*
- {//
-    id: "kdr-100-3",
-    name: "KDR 100.3",
-    streamUrl: "https://api.instant.audio/data/streams/132/kdr",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/kdr.png",
- },*/
+ },
  {
     id: "kkcr-91-7-fm",
     name: "KKCR 91.7 FM",
     streamUrl: "https://kkcradio.urdt.net:8000",
     logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/kkcr.png",
- },/*
- {//not yet
-    id: "radio-kwemala",
-    name: "Radio Kwemala",
-    streamUrl: "https://api.instant.audio/data/streams/132/kwemala",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/kwemala.png",
- },*/
+ },
  {
     id: "mcf-radio-kampala",
     name: "MCF Radio Kampala",
     streamUrl: "https://streams.radio.co/s79fbbb432/listen",
     logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/mcf.png",
- },/*
- {//not yet
-    id: "radio-rukungiri-96-9",
-    name: "Radio Rukungiri 96.9 FM",
-    streamUrl: "https://api.instant.audio/data/streams/132/rukungiri",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/rukungiri.png",
- },*
- {
-    id: "street-deejays-radio",
-    name: "Street Deejays",
-    streamUrl: "https://stream-153.zeno.fm/nbwdnxz7na0uv",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/street-deejays.png",
  },
- {//not yet
-    id: "vega-radio-ug",
-    name: "Vega Radio UG",
-    streamUrl: "https://api.instant.audio/data/streams/132/vega",
-    logoUrl: "https://cdn.instant.audio/images/logos/radio-co-ug/vega.png",
- },*/
  {
     id: "beat-fm",
     name: "Beat FM",
